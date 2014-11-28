9f358b63d51c02c8dbc0799314468d00271676bd
cyrusmith <info@interosite.ru>
2014-11-28 15:37:33 +0500

    +get ready to update

--- www/js/dropbike.constants.js
+++ www/js/dropbike.constants.js
@@ -6,7 +6,7 @@ define([
 
     angular.module('dropbike')
 
-        .constant("GEO_ACCURACY", 100) //meters
+        .constant("GEO_ACCURACY", 50) //meters
 
         .constant("FACEBOOK_ID", "297052723828418")
         .constant("FACEBOOK_REDIRECT_URL", "http://ec2-54-68-46-169.us-west-2.compute.amazonaws.com:8080")
@@ -14,9 +14,9 @@ define([
         .constant("WEBSITE", "http://dropbyke.com")
         .constant("BANNER_IMAGE", "http://ec2-54-68-46-169.us-west-2.compute.amazonaws.com:8080/DropbikeBackend-0.1/images/banner.jpg")
 
-        .constant('BACKEND_URL', 'http://192.168.1.103:8081/DropbikeBackend') //local
+        //.constant('BACKEND_URL', 'http://192.168.1.103:8081/DropbikeBackend') //local
         //.constant('BACKEND_URL', 'http://ec2-54-69-186-125.us-west-2.compute.amazonaws.com:8080/DropbikeBackend-0.1') //dev
-        //.constant('BACKEND_URL', 'http://ec2-54-68-46-169.us-west-2.compute.amazonaws.com:8080/DropbikeBackend-0.1') //prod
+        .constant('BACKEND_URL', 'http://ec2-54-68-46-169.us-west-2.compute.amazonaws.com:8080/DropbikeBackend-0.1') //prod
 
         .constant('GOOGLE_API_KEY', 'AIzaSyAzMonYKYPE18hIqNSyRF422iCv4WdU8e4');
 
